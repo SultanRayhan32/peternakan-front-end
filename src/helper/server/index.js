aa1efc1f04ef023038d326f0e4a15df4fa7be039
bintangmau <bintangmaulanahabib@gmail.com>
2020-11-17 14:44:50 +0700

    pull fusion v1

--- src/helper/server/index.js
+++ src/helper/server/index.js
@@ -1,4 +1,4 @@
 
-// export const api = 'http://localhost:2001/'
-const api = 'https://e036c3348052.ngrok.io/'
+export const api = 'http://localhost:2001/'
+// const api = 'https://e036c3348052.ngrok.io/'
 export default api
